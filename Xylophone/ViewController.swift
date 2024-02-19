8e181f0aab69bd85ed5d832533a8677cb62fbe31
Kim Thanh Bui <thanh.kimm.bui@gmail.com>
2024-02-18 19:54:39 -0800

    Initial commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print(sender)
+        print(sender.backgroundColor)
+        print(sender.currentBackgroundImage)
+        playSound()
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
+    
 }
 
